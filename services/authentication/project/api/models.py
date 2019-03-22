e89471cd797532d888ee76f8f9bd0407301e7bd9
Thomas Van Bogaert <thomas.vanbogaert@student.uantwerpen.be>
2019-03-22 11:19:33 +0100

    (Temporarily) give up on saving salt as unicode. Use string of integer instead.

--- services/authentication/project/api/models.py
+++ services/authentication/project/api/models.py
@@ -11,7 +11,7 @@ class Password(db.Model):
 
     user_id = db.Column(db.Integer, primary_key=True)
     password_hash = db.Column(db.String(128), nullable=False)
-    salt = db.Column(db.String(64), nullable=False)
+    salt = db.Column(db.String(128), nullable=False)
     auth_allowed = db.Column(db.Boolean(), default=True, nullable=False)
 
     def __init__(self, user_id, password):
@@ -20,14 +20,15 @@ class Password(db.Model):
 
     def set_password(self, password):
         random_val = str(SystemRandom().getrandbits(32*8))  # generate a salt with 32 random bytes
-        random_str = ""
-        # Python chr(i) supports input from 0 to 1114111, so at least 6 digits/char allowed
-        for i in range(0, len(random_val), 6):
-            char_nr = int("".join(random_val[i + k] for k in range(6) if i + k < len(random_val)))
-            char = chr(char_nr + 32)
-            if unicodedata.category(char) not in 'cC':  # not a control character
-                random_str += char
-        self.salt = random_str
+        # random_str = ""
+        # # Python chr(i) supports input from 0 to 1114111, so at least 6 digits/char allowed
+        # for i in range(0, len(random_val), 6):
+        #     char_nr = int("".join(random_val[i + k] for k in range(6) if i + k < len(random_val)))
+        #     char = chr(char_nr + 32)
+        #     if unicodedata.category(char) not in 'cC':  # not a control character
+        #         random_str += char
+        # self.salt = random_str
+        self.salt = random_val
         self.password_hash = pwd_context.hash(self.salt + password)
 
     def verify_password(self, password):
