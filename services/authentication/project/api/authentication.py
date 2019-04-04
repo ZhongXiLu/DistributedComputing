e3d8d5672ca84534d8caa3e8827f9403de440885
Thomas Van Bogaert <thomas.vanbogaert@student.uantwerpen.be>
2019-04-04 22:56:51 +0200

    Implemented authentication service.

--- services/authentication/project/api/authentication.py
+++ services/authentication/project/api/authentication.py
@@ -1,7 +1,7 @@
 # services/users/project/api/users.py
-import numbers
+import requests
 
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, g
 from sqlalchemy import exc
 
 from project.api.models import Password
@@ -49,8 +49,7 @@ Authenticate
 """
 
 
-
-@authentication_blueprint.route('/authentication/ping', methods=['GET'])
+@authentication_blueprint.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
         'status': 'success',
@@ -58,7 +57,16 @@ def ping_pong():
     })
 
 
-@authentication_blueprint.route('/authentication', methods=['POST'])
+@authentication_blueprint.route('/ping_auth', methods=['GET'])
+@auth.login_required
+def ping_pong_login_required():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
+
+
+@authentication_blueprint.route('/passwords', methods=['POST'])
 def add_password():
     post_data = request.get_json()
     response_object = {
@@ -85,9 +93,6 @@ def add_password():
             response_object['status'] = 'success'
             response_object['message'] = f'{user_id}\'s password was added!'
 
-            # pw1 = Password.query.filter_by(user_id=user_id).first()
-            # print(pw1.to_json())
-
             return jsonify(response_object), 201
         else:
             response_object['message'] = f'User {user_id} already exists.'
@@ -97,50 +102,87 @@ def add_password():
         return jsonify(response_object), 400
 
 
-@authentication_blueprint.route('/authentication/ping2', methods=['GET'])
+@authentication_blueprint.route('/passwords', methods=['PUT'])
 @auth.login_required
-def ping_pong_login_required():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
-
-@auth.verify_password
-def verify_password(user_id, password):
-    # pw = Password.verify_auth_token(user_id)
-    pw = Password.query.filter_by(user_id=int(user_id)).first()
-    if not pw or not pw.verify_password(password):
-        return False
-    return True
-
-
-@authentication_blueprint.route('/authentication/verify_password')
-def verify_password_rest(user_id, password):
-    pass
-
-
-'''@authentication_blueprint.route('/users/<user_id>', methods=['GET'])
-def get_single_user(user_id):
-    """Get single user details"""
+def update_password():
+    put_data = request.get_json()
     response_object = {
         'status': 'fail',
-        'message': 'User does not exist'
+        'message': 'Invalid payload.'
+    }
+    if not put_data:
+        response_object['error'] = 'No data received.'
+        return jsonify(response_object), 400
+    password = put_data.get('password')
+    if password is None:
+        response_object['error'] = 'Password is None.'
+        return jsonify(response_object), 400
+    try:
+        pw = Password.query.filter_by(user_id=auth.username()).first()  # type: Password
+        if pw:
+            pw.set_password(password)
+            db.session.commit()
+            response_object['status'] = 'success'
+            response_object['message'] = f'Changed password of user {auth.username()}'
+            return jsonify(response_object), 200
+    except exc.IntegrityError:
+        db.session.rollback()
+    return jsonify(response_object), 400
+
+
+@authentication_blueprint.route('/passwords', methods=['DELETE'])
+@auth.login_required
+def delete_password():
+    response_object = {
+        'status': 'fail',
+        'message': 'Invalid payload.'
     }
     try:
-        user = Password.query.filter_by(id=int(user_id)).first()
-        if not user:
-            return jsonify(response_object), 404
-        else:
-            response_object = {
-                'status': 'success',
-                'data': {
-                    'id': user.id,
-                    'username': user.username,
-                    'email': user.email,
-                    'active': user.active
-                }
-            }
+        pw = Password.query.filter_by(user_id=auth.username()).first()  # type: Password
+        if pw:
+            db.session.delete(pw)
+            db.session.commit()
+            response_object['status'] = 'success'
+            response_object['message'] = f'Deleted password of user {auth.username()}'
             return jsonify(response_object), 200
-    except ValueError:
-        return jsonify(response_object), 404'''
+    except exc.IntegrityError:
+        db.session.rollback()
+    return jsonify(response_object), 400
+
+
+@authentication_blueprint.route('/verify_credentials', methods=['GET'])
+@auth.login_required
+def verify_credentials():
+    return jsonify({
+        'status': 'success',
+        'authorized': True
+    }), 200
+
+
+@authentication_blueprint.route('/token', methods=['GET'])
+@auth.login_required
+def get_auth_token():
+    pw = g.pw  # type: Password
+    token = pw.generate_auth_token()
+    return jsonify({
+        'status': 'success',
+        'token': token.decode('ascii')
+    })
+
+
+@auth.verify_password
+def verify_password(user_id_or_token, password):
+    # Try to authenticate with token
+    pw = Password.verify_auth_token(user_id_or_token)
+    if not pw:
+        # Try to authenticate with user_id + pw
+        try:
+            pw = Password.query.filter_by(user_id=int(user_id_or_token)).first()
+        except ValueError:
+            pass
+        if not pw or not pw.verify_password(password):
+            return False
+    g.pw = pw
+    return True
+
 
