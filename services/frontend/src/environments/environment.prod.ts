d60ae63cee012a2200434ef4625f47bed741e885
ZhongXiLu <zhongxilu@hotmail.com>
2019-04-18 11:00:56 +0200

    Added base urls for services in environment.ts + updated admin page

--- services/frontend/src/environments/environment.prod.ts
+++ services/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
   production: true,
   userServiceUrl: 'http://localhost:5001',
-  authServiceUrl: 'http://localhost:5004'
+  postServiceUrl: 'http://localhost:5002',
+  tagServiceUrl: 'http://localhost:5003',
+  authServiceUrl: 'http://localhost:5004',
+  likeServiceUrl: 'http://localhost:5005',
+  commentServiceUrl: 'http://localhost:5006',
+  newsfeedServiceUrl: 'http://localhost:5007',
+  antiCyberbullyingServiceUrl: 'http://localhost:5008',
+  adServiceUrl: 'http://localhost:5009'
 };
--- services/frontend/src/environments/environment.ts
+++ services/frontend/src/environments/environment.ts
@@ -5,7 +5,14 @@
 export const environment = {
   production: false,
   userServiceUrl: 'http://localhost:5001',
-  authServiceUrl: 'http://localhost:5004'
+  postServiceUrl: 'http://localhost:5002',
+  tagServiceUrl: 'http://localhost:5003',
+  authServiceUrl: 'http://localhost:5004',
+  likeServiceUrl: 'http://localhost:5005',
+  commentServiceUrl: 'http://localhost:5006',
+  newsfeedServiceUrl: 'http://localhost:5007',
+  antiCyberbullyingServiceUrl: 'http://localhost:5008',
+  adServiceUrl: 'http://localhost:5009'
 };
 
 /*
