f6f0fe05f880b3cfd782b01cea552ed067e3946b
ZhongXiLu <zhongxilu@hotmail.com>
2019-05-15 14:59:51 +0200

    Added poll service

---
+++ services/poll/main.py
@@ -0,0 +1,73 @@
+
+import schedule
+import time
+import requests
+
+from util.send_request import *
+
+# Services to be polled with their ping path
+# IMPORTANT: assume user, authentication and notification service are ALWAYS up
+SERVICES = [
+    ("ad", "ads/ping"),
+    ("anti-cyberbullying", "anti_cyberbullying/ping"),
+    ("comment", "comments/ping"),
+    ("follow", "follow/ping"),
+    ("friend", "friend/ping"),
+    ("like", "likes/ping"),
+    ("message", "message/ping"),
+    ("post", "posts/ping"),
+    ("tag", "tags/ping")
+]
+
+# Keep dict of statuses of services
+# => prevent sending the same notifications again every 30 seconds
+# False = service down
+service_status = {}
+
+
+def send_notification_to_all(service):
+    """Send notification to all users that a service is temporary down"""
+
+    response_obj = send_request('get', 'users', 'users', timeout=3)
+    all_users = [user['id'] for user in response_obj.json['data']['users']]
+
+    send_request('post', 'notification', 'notifications', timeout=3,
+                 json={'content': f'{service} service is temporary down. Please wait while we\'ll try to fix this.',
+                       'recipients': all_users}, auth=('2', 'admin'))
+
+
+def poll_services():
+    """Poll each service by pinging them"""
+
+    for service, path in SERVICES:
+        try:
+            response_object = send_request('get', service, f'{path}', timeout=3)
+            if response_object.json['status'] == "fail":
+                if not service_status[service]:
+                    service_status[service] = True  # status = down
+                    send_notification_to_all(service)
+            else:
+                service_status[service] = False     # status = up
+        except:
+            if not service_status[service]:
+                service_status[service] = True  # status = down
+                send_notification_to_all(service)
+
+
+def main():
+    """
+        Poll every service every 30 seconds
+        IMPORTANT: assume user, authentication and notification service are ALWAYS up
+    """
+
+    for service, path in SERVICES:
+        service_status[service] = False
+
+    schedule.every(30).seconds.do(poll_services)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ services/poll/util/verify_password.py
@@ -0,0 +1,37 @@
+import os
+import requests
+from flask import g
+from flask_httpauth import HTTPBasicAuth
+from util.send_request import send_request
+
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_password(user_id_or_token, password):
+    if os.environ.get('TESTING') == "True":
+        g.user_id_or_token = ""
+        g.password = ""
+        return True
+    response = send_request(
+        'get', 'authentication', 'verify_credentials', timeout=3, auth=(user_id_or_token, password))
+    if response.status_code == 401:
+        g.reason = 'Wrong credentials'
+        return False
+    elif response.status_code != 200:
+        g.reason = 'Other reason'
+        return False
+    try:
+        g.user_id = response.json['user_id']
+    except KeyError:
+        g.user_id = None
+    g.user_id_or_token = user_id_or_token
+    g.password = password
+    return True
+
+
+def is_admin(user_id):
+    response = send_request('put', 'authentication', 'is_admin', timeout=3, json={'user_id': user_id})
+    return response.json['is_admin']
+
+login_decorator = auth.login_required
