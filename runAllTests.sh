a495d3907271586cad5951277c9e34f52892ee01
ZhongXiLu <zhongxilu@hotmail.com>
2019-05-22 15:06:28 +0200

    Updated test and recreate_db script to work with kubernetes

--- runAllTests.sh
+++ runAllTests.sh
@@ -2,5 +2,6 @@
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 for SERVICE in ${ROOT_DIR}/services/*
 do
-    docker-compose -f ${ROOT_DIR}/docker-compose-dev.yml exec $(basename ${SERVICE}) python manage.py test
+    #docker-compose -f ${ROOT_DIR}/docker-compose-dev.yml exec $(basename ${SERVICE}) python manage.py test
+    kubectl exec -it $(kubectl get pods --all-namespaces | grep $(basename ${SERVICE})'-' | awk '{print $2}') python manage.py test
 done
